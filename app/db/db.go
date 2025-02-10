62ea823c501cf6417f20dcab0570aef24a292c84
mickaelyoshua7674 <mickaelyoshua@gmail.com>
2025-02-10 16:36:29 -0300

    Successfull connection to database

--- app/db/db.go
+++ app/db/db.go
@@ -12,6 +12,11 @@ import (
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
 
+type Film struct {
+	Id string `bson:"id"`
+	Title string `bson:"title"`
+}
+
 func main() {
 	uri := os.Getenv("MONGODB_URI")
 	docs := "www.mongodb.com/docs/drivers/go/current/"
@@ -31,8 +36,20 @@ func main() {
 		}
 	}()
 	
-	coll := client.Database("sample_mflix").Collection("movies")
 	title := "Back to the Future"
+	coll := client.Database("sample_mflix").Collection("movies")
+	/*
+	writeResult, err := coll.InsertOne(context.TODO(), Film{"id1", title})
+	if err != nil {
+		panic(err)
+	}
+	writeJsonData, err := json.MarshalIndent(writeResult, "", "	")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s\n", writeJsonData)
+	fmt.Println("Write result:", writeResult)
+	*/
 	var result bson.M
 	err = coll.FindOne(context.TODO(), bson.D{{Key: "title", Value: title}}).Decode(&result)
 	if err == mongo.ErrNoDocuments {
